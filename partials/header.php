eb58cd5a8ba02f618c24b2a5a7f23a9d38d23fce
Iceroller <Iceman_R@list.ru>
2016-11-15 15:02:23 +0200

    adddddddddddd alot

---
+++ partials/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <div id="header">
+        <strong id="logo">
+                   <a href="homepage.php">Beauty Main</a>
+        </strong>
+        <div class="menu">
+            <ul>
+                <li><a href="homepage.php">Home</a></li>
+                <li><a href="#">Search</a></li>
+                <li><a href="#">Service</a></li>
+                <li><a href="#">Info</a></li>
+                <li><a href="#">Contact us</a></li>
+            </ul>
+        </div>
+        <div class="log"></div>
+    </div>
+</body>
+</html>
